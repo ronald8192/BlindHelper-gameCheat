2ead035a768bfdb3376d9b694e7eb7dda89749c7
ronald8192 <u23805@gmail.com>
2015-11-18 01:44:56 +0800

    use data attirbute

--- js/blindHelper.js
+++ js/blindHelper.js
@@ -1,16 +1,7 @@
 /*Author: ronald8192*/
 var blindhelper = setInterval(function(){
-	var general = $("#box > span").first().attr("style");
-	var ans = -1, hitCount = 0, boxCount = $("#box > span").length;
-	for (var i = 1; i < boxCount; i++) {
-		if(general !== $("#box > span").eq(i).attr("style")) {
-			ans = i;
-			if(++hitCount > 1) {
-				ans = 0;
-				break;
-			}
-		}
-		if(ans != -1 && i > 2) break;
-	};
-	$("#box > span").eq(ans).trigger("click");
+	$("#box").find("span").each(function(i,e){
+		if($(e).data("type")=="a"){$(this).click();return false;}
+	});
 },1);
+
